8b5b609ab3dfe20258a639833960391a7f5e3077
thecodingunicorn <theunicorncoder@gmail.com>
2022-02-11 17:52:08 +0530

    added TDD Boilder plate

---
+++ strings and arrays/#1 isUnique.js
@@ -0,0 +1,13 @@
+/**
+ * Implement an algorithm to determine if a string has all unique characters. what if you cannot use additional data structure?
+ *
+ * @param {string} str
+ * @return {boolean}
+ *
+ */
+
+function isUnique() {
+  return 1;
+}
+
+module.exports = isUnique;
---
+++ tests/#1 isUnique.test.js
@@ -0,0 +1,7 @@
+const isUnique = require("../strings and arrays/#1 isUnique");
+
+describe("#1 isUnique", () => {
+  it("test case #1", () => {
+    expect(isUnique()).toBe(1);
+  });
+});
